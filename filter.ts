c2877e0298e3d40d02f2ebacc93d955b8abe004c
Iggr Smirnov <uchetzapis@yahoo.com>
2022-05-31 22:26:22 +0300

    green test

--- filter.ts
+++ filter.ts
@@ -6,6 +6,18 @@ export type Course = {
 }
 
 
-export function priceFilter(courses: Course[], prices: PriceRange): Course[] {
-    return courses;
+export function priceFilter(courses: Course[], searchRange: PriceRange): Course[] {
+    function isMatch(course: Course, [lowerSearchPrice, upperSearchPrice]: PriceRange): boolean {
+        const [lowerCoursePrice, upperCoursePrice] = course.prices;
+
+        if (lowerSearchPrice !== null && upperCoursePrice !== null && lowerSearchPrice > upperCoursePrice) {
+            return false;
+        }
+        if (upperSearchPrice !== null && lowerCoursePrice !== null && upperSearchPrice < lowerCoursePrice) {
+            return false;
+        }
+        return true;
+
+    }
+    return courses.filter((course) => isMatch(course, searchRange));
 }
