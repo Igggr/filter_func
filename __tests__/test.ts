1300da3f43da2f99edb2280c284f1666aacfa3ea
Iggr Smirnov <uchetzapis@yahoo.com>
2022-05-31 22:34:47 +0300

    Add 2 more tests

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -12,12 +12,6 @@ let courses: Course[] = [
     { name: "Courses in France", prices: [null, null] },
 ];
 
-  // Варианты цен (фильтры), которые ищет пользователь
-  let requiredRange1 = [null, 200];
-  let requiredRange2 = [100, 350];
-  let requiredRange3 = [200, null];
-
-
 describe('filter by price', () => {
   it('[100, 350]', () => {
     const requiredRange: PriceRange = [100, 350];
@@ -30,8 +24,41 @@ describe('filter by price', () => {
       { name: "Courses in Kazakhstan", prices: [56, 324] },
       { name: "Courses in France", prices: [null, null] },
     ];
+
     const actual = priceFilter(courses, requiredRange)
     expect(actual).toEqual(expected);
+  });
+
+  it('[null, 200]', () => {
+    const requiredRange: PriceRange = [null, 200];
+    const expected = [
+      { name: "Courses in England", prices: [0, 100] }, 
+      { name: "Courses in Italy", prices: [100, 200] }, 
+      { name: "Courses in Russia", prices: [null, 400] },
+      { name: "Courses in China", prices: [50, 250] },
+      { name: "Courses in USA", prices: [200, null] },
+      { name: "Courses in Kazakhstan", prices: [56, 324] },
+      { name: "Courses in France", prices: [null, null] },
+  ];
+
+  const actual = priceFilter(courses, requiredRange)
+  expect(actual).toEqual(expected);
+  });
+
+  it('[200, null]', () => {
+    const requiredRange: PriceRange = [200, null];
+    const expected = [
+      { name: "Courses in Germany", prices: [500, null] }, 
+      { name: "Courses in Italy", prices: [100, 200] }, 
+      { name: "Courses in Russia", prices: [null, 400] },
+      { name: "Courses in China", prices: [50, 250] },
+      { name: "Courses in USA", prices: [200, null] },
+      { name: "Courses in Kazakhstan", prices: [56, 324] },
+      { name: "Courses in France", prices: [null, null] },
+  ];
+
+  const actual = priceFilter(courses, requiredRange)
+  expect(actual).toEqual(expected);
   });
 
 });
